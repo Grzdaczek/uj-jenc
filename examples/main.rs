7a4d24216dda8cf84e058debcd250e23f4d6eb81
Grzdaczek <grzegorz.jnsk@gmail.com>
2022-01-10 17:55:29 +0100

    Static tables for cosine multipliers

--- examples/main.rs
+++ examples/main.rs
@@ -2,12 +2,12 @@ use std::fs;
 use uj_jenc::codec::*;
 
 fn main() {
-    let file = fs::read("./examples/in_image.ppm").unwrap();
+    let file = fs::read("./examples/in_1024x1280.ppm").unwrap();
     let mut rcr_data = Vec::new();
     let mut ppm_data = Vec::new();
 
     let img = ppm::decode(&mut &file[..]);
-    rcr::encode(&mut rcr_data, rcr::Settings::quality(10), &img.into());
+    rcr::encode(&mut rcr_data, rcr::Settings::quality(7), &img.into());
 
     let img = rcr::decode(&mut &rcr_data[..]);
     ppm::encode(&mut ppm_data, &img.into());
